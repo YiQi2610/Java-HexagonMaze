f82394036639d3dc6ba657770e3e3e185379d33a
Ker Chee TUNG <ker.tung@telecom-paris.fr>
2023-02-25 00:22:11 +0100

    finished all commments

--- projetJava/src/view/menu/Item/QuitMenuItem.java
+++ projetJava/src/view/menu/Item/QuitMenuItem.java
@@ -13,6 +13,10 @@ public class QuitMenuItem extends JMenuItem implements ActionListener{
 	
 	private final MazeFenetre mazeFenetre;
 	
+	/**
+	 * Quit Menu Item to quit the program
+	 * @param mazeFenetre
+	 */
 	public QuitMenuItem(MazeFenetre mazeFenetre) {
 		super("Quit");
 		
@@ -22,22 +26,35 @@ public class QuitMenuItem extends JMenuItem implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		/**
+		 * If the labyrinth is modified, it shows an option dialog to ask user if user wants to save labyrinth
+		 * Provide yes, no, cancel options
+		 */
 		if(mazeFenetre.getMazeModel().isModified()) {
-        	int input = JOptionPane.showInternalOptionDialog(this, "Drawing not saved. Save it?","Quit application", 
+        	int input = JOptionPane.showInternalOptionDialog(this, "Labyrinth not saved. Save it?","Quit application", 
         				JOptionPane.YES_NO_CANCEL_OPTION,
         				JOptionPane.WARNING_MESSAGE,
         				null,null,null);
+        	
+        	// If user choose "No", the program will be closed directly
     		if(input==JOptionPane.NO_OPTION ){
     			System.exit(0);
     		}
+    		
+    		// If user choose "Cancel", the dialog will be closed and user back to interface
     		else if(input==JOptionPane.CANCEL_OPTION ) {
     			return;
     		}
+    		
+    		// If user choose "Ok", an input dialog is shown to ask user to type a file name to save labyrinth
     		else if(input==JOptionPane.OK_OPTION) {
     			String fileName = JOptionPane.showInputDialog( "Enter file name :" );
+    			// Add extension ".txt" at the end of filename given
     			fileName = fileName +".txt";
     			try {
+    				// call function saveToTextFile() from mazemodel to save current labyrinth into text file
     				mazeFenetre.getMazeModel().saveToTextFile(fileName);
+    				// Show a message dialog for confirmation of saving 
     				JOptionPane.showMessageDialog(null, "Your maze is saved as '" + fileName + " '.");
     			} catch (FileNotFoundException e1) {
     				e1.printStackTrace();
@@ -46,6 +63,8 @@ public class QuitMenuItem extends JMenuItem implements ActionListener{
     			}
     		}
     	}
+		
+		//If labyrinth is not modified, interface will be closed directly
     	System.exit(0);
 		
 	}
--- projetJava/src/view/menu/Item/SaveMenuItem.java
+++ projetJava/src/view/menu/Item/SaveMenuItem.java
@@ -12,6 +12,10 @@ import view.MazeFenetre;
 public class SaveMenuItem extends JMenuItem implements ActionListener{
 	private final MazeFenetre mazeFenetre;
 	
+	/**
+	 * SaveMenuItem to save current labyrinth into a text file
+	 * @param mazeFenetre
+	 */
 	public SaveMenuItem(MazeFenetre mazeFenetre) {
 		super("Save");
 		
@@ -20,14 +24,25 @@ public class SaveMenuItem extends JMenuItem implements ActionListener{
 	}
 
 	@Override
+	/**
+	 * When user clicks on this menu item, a confirm dialog is shown to ask user for saving labyrinth
+	 */
 	public void actionPerformed(ActionEvent e) {
 		int input = JOptionPane.showConfirmDialog(null, "Are you sure to save this maze?");
+		
+		//If user chooses "No" or "Cancel", labyrinth will not be saved and user back to interface
 		if(input==1 || input==2){return;}
+		
+		//If user chooses "Yes" to save labyrinth
 		else if(input==0) {
+			
+			//An input dialog is shown to ask user to enter file name
 			String fileName = JOptionPane.showInputDialog( "Enter file name :" );
+			
+			//If user clicks cancel, labyrinth will not be saved and user back to interface, to detect if user has clicked "Cancel", we see if fileName is null
 			if(fileName==null) {return;}
 			fileName = fileName +".txt";
-			try {
+			try {// If filename is not null, labyrinth will be saved in a text file with filename entered 
 				mazeFenetre.getMazeModel().saveToTextFile(fileName);
 				JOptionPane.showMessageDialog(null, "Your maze is saved as '" + fileName + " '.");
 			} catch (FileNotFoundException e1) {
